9dcc5fefc581a5efee3cca182c26d167c4224985
lau-ren <colls.lauren@gmail.com>
2016-05-16 17:37:28 +1200

    start flip function

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
 
+
+/* required modules*/
+var flip = require('../lib/flip.js')
+
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Flipper' });
 });
+
+
+/* POST result. */
+router.post('/', function(req, res, next) {
+
+  // console.log(req.body, "this is all the text")
+
+  flip(req.body.text)
+  res.render('index', { title: 'Flipper' });
+});
+
 
 module.exports = router;

79c04c87fda613977385652eb191d8f516e36c9b
lau-ren <colls.lauren@gmail.com>
2016-05-19 17:53:37 +1200

    modularise tests for ea function

---
+++ tests/tag-tests.js
@@ -0,0 +1,21 @@
+var test = require('tape');
+var isArray =require('isarray')
+
+var tag = require('../lib/tag.js')
+
+
+// test the tag function
+test('tag function tags each word to the string input', function(t){
+	// arrange
+	var inputStr = "Her his"
+	var expectedResult =  [['her', 'PRP$'], ['his', 'PRP$']]
+
+	// action
+	var actualResult = tag(inputStr)
+
+	// assert
+	t.true(isArray(actualResult), 'tag func returns an array')
+	t.deepEqual(actualResult, expectedResult, 'tag func turns what is expected')
+	t.equal(actualResult.length, expectedResult.length, 'tag func return array with correct length')
+	t.end()
+})
--- tests/tests.js
+++ tests/tests.js
@@ -6,44 +6,44 @@ var tag = require('../lib/tag.js')
 
 
 
-// test the tag function
-test('tag function tags each word to the string input', function(t){
-	// arrange
-	var inputStr = "Her his"
-	var expectedResult =  [['her', 'PRP$'], ['his', 'PRP$']]
+// // test the tag function
+// test('tag function tags each word to the string input', function(t){
+// 	// arrange
+// 	var inputStr = "Her his"
+// 	var expectedResult =  [['her', 'PRP$'], ['his', 'PRP$']]
 
-	// action
-	var actualResult = tag(inputStr, flip)
+// 	// action
+// 	var actualResult = tag(inputStr, flip)
 
-	// assert
-	t.true(isArray(actualResult), 'tag func returns an array')
-	t.deepEqual(actualResult, expectedResult, 'tag func turns what is expected')
-	t.equal(actualResult.length, expectedResult.length, 'tag func return array with correct length')
-	t.end()
-})
+// 	// assert
+// 	t.true(isArray(actualResult), 'tag func returns an array')
+// 	t.deepEqual(actualResult, expectedResult, 'tag func turns what is expected')
+// 	t.equal(actualResult.length, expectedResult.length, 'tag func return array with correct length')
+// 	t.end()
+// })
 
-// test the flip function
-test('flip function flips the pronouns in input strng', function(t){
-	// arrange
-	var inputArr =  [ [ 'he', 'PRP' ],
-							      [ 'said', 'VBD' ],
-							      [ 'that', 'IN' ],
-							      [ 'is', 'VBZ' ],
-							      [ 'hers', 'PRP' ]
-							    ]
+// // test the flip function
+// test('flip function flips the pronouns in input strng', function(t){
+// 	// arrange
+// 	var inputArr =  [ [ 'he', 'PRP' ],
+// 							      [ 'said', 'VBD' ],
+// 							      [ 'that', 'IN' ],
+// 							      [ 'is', 'VBZ' ],
+// 							      [ 'hers', 'PRP' ]
+// 							    ]
 
-	var expectedResult ='she said that is his'
+// 	var expectedResult ='she said that is his'
 
-	// action
-	var actualResult = flip(inputArr)
+// 	// action
+// 	var actualResult = flip(inputArr)
 	
-	// assert
-	t.ok(actualResult, 'flip returns something, i think')
-	t.true(typeof(actualResult) === 'string', 'flip returns a string')
-	t.deepEqual(actualResult,expectedResult,'flip retuns what was expected')
+// 	// assert
+// 	t.ok(actualResult, 'flip returns something, i think')
+// 	t.true(typeof(actualResult) === 'string', 'flip returns a string')
+// 	t.deepEqual(actualResult,expectedResult,'flip retuns what was expected')
 
-	t.end()
-})
+// 	t.end()
+// })
 
 test('', function(t){
 	// arrange

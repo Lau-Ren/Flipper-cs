f8bff69a8cfc200923deab114cbe976c4cec0193
lau-ren <colls.lauren@gmail.com>
2016-07-09 21:38:05 +1200

    add to deploy

--- server.js
+++ server.js
@@ -9,29 +9,25 @@ const app = express();
 const router = express.Router()
 const bodyParser = require('body-parser')
 
-// get port from environment and store in Express
 const port = '3000'
 
-// create HTTP server
 var server = http.createServer(app)
 
 app.set('port', port);
 app.use(express.static(path.join(__dirname, 'public')));
-server.listen(port)
-router.use(bodyParser.json()); // for parsing application/json
-router.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
-
-// routes go below:
+server.listen(process.env.PORT || port, function() {
+  console.log("listening on port 3000")
+})
+router.use(bodyParser.json()); 
+router.use(bodyParser.urlencoded({ extended: true })); 
 
 const getTweets = require('./lib/get-tweets.js')
 const retweet = require('./lib/retweet.js')
 
-//get homepage
 router.get('/', function(req, res, next) {
   res.render('index')
 })
 
-//get tweets
 router.get('/api/tweets', function(req, res, next) {
 
   let twitterHandle = req.query.twitterHandle
@@ -42,9 +38,7 @@ router.get('/api/tweets', function(req, res, next) {
     })
 })
 
-//retweet a flipped tweet or text string
 router.post('/retweet', function(req, res, next) {
-  // let twitterHandle = req.query.twitterHandle
 var status = req.body
 var userhandle = req.body
   retweet(status, userhandle)
@@ -53,6 +47,5 @@ var userhandle = req.body
       return res.json(tweets)
     })
 })
-
 
 app.use('/', router)
